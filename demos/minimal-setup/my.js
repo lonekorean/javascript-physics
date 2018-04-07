7d05b644dce93ada417388dad68d25465e38374b
Will Boyd <will@codersblock.com>
2018-04-07 08:17:59 -0400

    Minimal setup demo

---
+++ demos/minimal-setup/my.js
@@ -0,0 +1,17 @@
+// engine
+let engine = Matter.Engine.create();
+
+// render
+let render = Matter.Render.create({
+    element: document.getElementById('matter'),
+    engine: engine
+});
+Matter.Render.run(render);
+
+// runner
+let runner = Matter.Runner.create();
+Matter.Runner.run(runner, engine);
+
+// draw
+let ball = Matter.Bodies.circle(400, 100, 50);
+Matter.World.add(engine.world, ball);
